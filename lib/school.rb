49d7a51a77296fab79b602231837d06c375b8a23
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-06 20:37:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,6 @@
 class School
 
-
-  def initialize(name)
-    @name = name
-  end
-  
-  def roster=(roster)
-    @roster = roster
-  end
-  
-  def roster
-    @roster = roster
-
-  end
-  
-  def add_student=(add_student, grade)
-    @add_student = add_student
-    @grade = grade
-  end
-  
+attr_accessor :name
   
 end
 

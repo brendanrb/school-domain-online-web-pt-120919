7cb608acf3d9c06cf4a4a41b3f2250bf6ab5a134
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-06 20:32:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster=(roster)
+    @roster = roster
+  end
+  
+  def roster
+    @roster = roster
+
+  end
+  
+  def add_student=(add_student, grade)
+    @add_student = add_student
+    @grade = grade
+  end
+  
+  
+end
+
